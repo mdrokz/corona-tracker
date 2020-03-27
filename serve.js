1ac4c12150a915c00081e3d5156759dba80b24aa
mdrokz <mohammadmunshi@gmail.com>
2020-03-27 14:03:48 +0530

    added custom js file for serving static files

---
+++ serve.js
@@ -0,0 +1,31 @@
+// var fs = require('fs'),
+//     http = require('http');
+
+// http.createServer(function (req, res) {
+//   fs.readFile('./out/index.html', function (err,data) {
+//     if (err) {
+//       res.writeHead(404);
+//       res.end(JSON.stringify(err));
+//       return;
+//     }
+//     res.writeHead(200);
+//     res.end(data);
+//   });
+// }).listen(8080, () => console.log("server is listening at port 8080"));
+
+var fs = require('fs');
+
+var http = require('http');
+
+var nStatic = require('node-static');
+
+var fileServer = new nStatic.Server('./out');
+
+var pageview = require('./views.json');
+
+http.createServer(function (req, res) {
+    pageview.count++;
+    fs.writeFileSync('./views.json', JSON.stringify(pageview));
+    fileServer.serve(req, res);
+
+}).listen(8080);
